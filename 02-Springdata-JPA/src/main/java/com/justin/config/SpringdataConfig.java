5f266ef9c77d8e194ecbf5b75e0c64c123761d79
justin.Zhang <justinzhangyf@gmail.com>
2022-02-03 17:46:07 +0800

    springdataJPA xml configuration to java config

---
+++ 02-Springdata-JPA/src/main/java/com/justin/config/SpringdataConfig.java
@@ -0,0 +1,53 @@
+package com.justin.config;
+
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories
+@EnableTransactionManagement
+public class SpringdataConfig {
+
+    @Bean
+    public DataSource dataSource() {
+
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl("jdbc:mysql://120.27.132.111:3306/springdata_jpa?characterEncoding=UTF-8");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root123");
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("com.acme.domain");
+        factory.setDataSource(dataSource());
+        return factory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory);
+        return txManager;
+    }
+}
--- 02-Springdata-JPA/src/test/java/com/justin/SpringdataJpaTest.java
+++ 02-Springdata-JPA/src/test/java/com/justin/SpringdataJpaTest.java
@@ -1,20 +1,22 @@
 package com.justin;
 
+import com.justin.config.SpringdataConfig;
 import com.justin.pojo.Customer;
 import com.justin.repositories.CustomerRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.annotation.Resource;
 import java.util.Optional;
 
-@ContextConfiguration("/Spring.xml")
+//@ContextConfiguration("/Spring.xml")
+@ContextConfiguration(classes = SpringdataConfig.class)
 @RunWith(SpringJUnit4ClassRunner.class)
 public class SpringdataJpaTest {
 
-    @Autowired
+    @Resource
     CustomerRepository customerRepository;
 
     @Test
@@ -22,6 +24,22 @@ public class SpringdataJpaTest {
         Optional<Customer> customerById = customerRepository.findById(1L);
         Customer customer = customerById.orElseGet(null);
         System.out.println(customer);
+    }
+
+    @Test
+    public void testC(){
+        Customer cust = new Customer("Gary", "Hospital");
+        customerRepository.save(cust);
+
+        customerRepository.findAll().forEach(System.out::println);
+    }
+
+    @Test
+    public void testU(){
+        Customer customer = customerRepository.findById(2L).orElseGet(null);
+        System.out.println(customer);
+        customer.setCustAddress("Hotel");
+        customerRepository.save(customer);
     }
 
 }
